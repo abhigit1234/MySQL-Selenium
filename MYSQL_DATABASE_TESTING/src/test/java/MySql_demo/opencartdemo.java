73ca1909e4e2b91329a8497a5c596e92e76afef3
tubby <tubby@LAPTOP-RH55RME4>
2023-11-16 11:59:31 +0530

    My-SQL Selenium

---
+++ MYSQL_DATABASE_TESTING/src/test/java/MySql_demo/opencartdemo.java
@@ -0,0 +1,84 @@
+package MySql_demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class opencartdemo {// http://localhost:8012/opencart/upload
+							// http://localhost:8012/opencart/upload/admin
+
+	@Test
+	public void opencart() throws InterruptedException, SQLException {
+
+		String cus_firstname = "ayaansh";
+		String cus_lastname = "naidu";
+		String cus_email ="ayaansh@londongmail.com";
+		String cus_password = "naiduabhi";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://localhost:8012/opencart/upload");
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.partialLinkText("Register")).click();
+		driver.findElement(By.name("firstname")).sendKeys(cus_firstname);
+		driver.findElement(By.name("lastname")).sendKeys(cus_lastname);
+		driver.findElement(By.name("email")).sendKeys(cus_email);
+		driver.findElement(By.name("password")).sendKeys(cus_password);
+
+		WebElement agree = driver.findElement(By.name("agree"));
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("arguments[0].click()", agree);
+		WebElement button = driver.findElement(By.xpath("//button[text()='Continue']"));
+		jse.executeScript("arguments[0].click()", button);
+
+		try {
+			String confirm = driver.findElement(By.xpath("//h1[text()='Your Account Has Been Created!']")).getText();
+			if (confirm.equals("Your Account Has Been Created!")) {
+				System.out.println("Registration successfull from UI/Application");
+			} else {
+				System.out.println("Registration not successfull");
+			}}
+		catch(Exception e) {
+				System.out.println(e.getMessage());
+		}
+		
+		
+
+
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3308/opencart","root",""); 
+		Statement st = con.createStatement();
+		String sql = "select firstname,lastname,email,password from oc_customer";
+		ResultSet rs =		st.executeQuery(sql);
+		
+		boolean status = false;
+		
+		while(rs.next()) {
+			String fname =	rs.getString("firstname");
+			String lname =	rs.getString("lastname");
+			String email =	rs.getString("email");
+			String pswd =	rs.getString("password");
+		
+		System.out.println(fname+" "+lname+" "+email);
+			if(cus_firstname.equals(fname) && cus_lastname.equals(lname) 
+					&& cus_email.equals(email)) {
+				System.out.println("Record found in table || Test Passed");
+				status=true;
+				break;
+			}	
+		
+		}
+		if(status==false) {
+			System.out.println("Record not found in table || Test Failed");
+		}
+			
+//	driver.close();
+	}
+}
